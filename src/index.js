6dfb35d41803a7d8530bdc9f12a39466f9290b1e
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-01-18 18:17:23 +0530

    feat: fetch weather data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./style.css";
+
+
+async function getWeatherData(location) {
+    const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?unitGroup=us&key=4BYVM8YVVQ3K5QRTX6ZENKAKG&contentType=json');
+    const weatherData = await response.json();
+    console.log("API response description :"+weatherData.days[0].description);
+    console.log("API response temp :"+weatherData.days[0].temp);
+    console.log("API response humidity:"+weatherData.days[0].humidity);
+    console.log("API response windspeed:"+weatherData.days[0].windspeed);
+    console.log("API response conditions:"+weatherData.days[0].conditions);
+}
+
+getWeatherData();
