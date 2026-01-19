1a54089ae5b284c8bee28a020cd9d33f0aa365b1
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-01-19 19:26:37 +0530

    feat: connect form to weather fetch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 import "./style.css";
 
+
+const cityInput = document.querySelector('#city-name');
+const getWeatherBtn = document.querySelector('#get-weather-btn');
+
+getWeatherBtn.addEventListener('click', function (e) {
+    
+    console.log("Button clicked");
+
+    if (cityInput.value == undefined || cityInput.value == "") {
+        return;
+    }
+
+    getWeatherData(cityInput.value).then((data) => {
+        console.log("desc  :" + data.getDesc());
+        console.log("temp  :" + data.getTemp());
+        console.log("humidity  :" + data.getHumidity());
+        console.log("windspeed  :" + data.getWindSpeed());
+        console.log("conditions  :" + data.getConditions());
+    });
+
+    e.preventDefault();
+
+});
+
+
 function weatherData(desc, temp, humidity, windspeed, conditions) {
 
     const getDesc = () => desc;
@@ -15,7 +40,15 @@ function weatherData(desc, temp, humidity, windspeed, conditions) {
 
 
 async function getWeatherData(location) {
-    const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?unitGroup=us&key=4BYVM8YVVQ3K5QRTX6ZENKAKG&contentType=json');
+
+
+    console.log("location " + location);
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=4BYVM8YVVQ3K5QRTX6ZENKAKG&contentType=json`);
+
+    //const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?unitGroup=us&key=4BYVM8YVVQ3K5QRTX6ZENKAKG&contentType=json');
+
+    console.log("status:", response.status);
+
     const apiResp = await response.json();
 
     const weatherInfo = weatherData(
@@ -25,17 +58,10 @@ async function getWeatherData(location) {
         apiResp.days[0].windspeed,
         apiResp.days[0].conditions
     );
-    
+
     return weatherInfo;
 }
 
 
-getWeatherData().then((data) => {
-    console.log("desc  :" + data.getDesc());
-    console.log("temp  :" + data.getTemp());
-    console.log("humidity  :" + data.getHumidity());
-    console.log("windspeed  :" + data.getWindSpeed());
-    console.log("conditions  :" + data.getConditions());
-});
 
-//console.log("getWeatherData  " + getWeatherData().getDesc());
+//getWeatherData("dubai");
