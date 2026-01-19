714997bd14c49266f7d5e045d19eb6b63ac93d74
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-01-19 18:19:21 +0530

    feat: add weatherData factory

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 import "./style.css";
 
+function weatherData(desc, temp, humidity, windspeed, conditions) {
+
+    const getDesc = () => desc;
+    const getTemp = () => temp;
+    const getHumidity = () => humidity;
+    const getWindSpeed = () => windspeed;
+    const getConditions = () => conditions;
+
+    return { getDesc, getTemp, getHumidity, getWindSpeed, getConditions };
+
+}
+
+
 
 async function getWeatherData(location) {
     const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?unitGroup=us&key=4BYVM8YVVQ3K5QRTX6ZENKAKG&contentType=json');
-    const weatherData = await response.json();
-    console.log("API response description :"+weatherData.days[0].description);
-    console.log("API response temp :"+weatherData.days[0].temp);
-    console.log("API response humidity:"+weatherData.days[0].humidity);
-    console.log("API response windspeed:"+weatherData.days[0].windspeed);
-    console.log("API response conditions:"+weatherData.days[0].conditions);
+    const apiResp = await response.json();
+
+    const weatherInfo = weatherData(
+        apiResp.days[0].description,
+        apiResp.days[0].temp,
+        apiResp.days[0].humidity,
+        apiResp.days[0].windspeed,
+        apiResp.days[0].conditions
+    );
+    
+    return weatherInfo;
 }
 
-getWeatherData();
+
+getWeatherData().then((data) => {
+    console.log("desc  :" + data.getDesc());
+    console.log("temp  :" + data.getTemp());
+    console.log("humidity  :" + data.getHumidity());
+    console.log("windspeed  :" + data.getWindSpeed());
+    console.log("conditions  :" + data.getConditions());
+});
+
+//console.log("getWeatherData  " + getWeatherData().getDesc());
